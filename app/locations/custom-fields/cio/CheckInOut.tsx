7a4359d76aec5faeec4d6f397b8850194e321ced
tanvee-dadhaneeya <tanvee.dadhaneeya@accenture.com>
2024-10-30 18:32:27 +0530

    remove entry lock after user save locked entry

--- app/locations/custom-fields/cio/CheckInOut.tsx
+++ app/locations/custom-fields/cio/CheckInOut.tsx
@@ -17,7 +17,9 @@ const CheckInOut = () => {
   const [dataLoading, setDataLoading] = React.useState<boolean>(false);
   const [buttonDisabled, setButtonDisabled] = React.useState<boolean>(true);
   const [currentMetaData, setCurrentMetaData] = React.useState<any>(null);
-  const [isModalOpen, setIsModalOpen] = React.useState(false)
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const [isReady, setIsReady] = React.useState(false);
   // Create a ref to hold the latest currentMetaData
   const currentMetaDataRef = React.useRef(currentMetaData);
 
@@ -220,6 +222,34 @@ const CheckInOut = () => {
       ),
     });
   };
+
+  const handleSaveEntry = React.useCallback(
+    async (): Promise<void> => {
+      try {
+        const entry = appSdk?.location?.CustomField?.entry;
+        if (!entry) return;
+
+        entry.onSave(() => {
+          unLockEntry();
+        });
+      } catch (error) {
+        console.error("Error saving entry:", error);
+      }
+    },
+    [appSdk]
+  );
+
+  React.useEffect(() => {
+    if (appSdk) {
+      setIsReady(true);
+    }
+  }, [appSdk]);
+
+  React.useEffect(() => {
+    if (isReady) {
+      handleSaveEntry();
+    }
+  }, [isReady, handleSaveEntry]);
 
   // Do not render output if appSdk is not available.
   if (!appSdk) return null;
