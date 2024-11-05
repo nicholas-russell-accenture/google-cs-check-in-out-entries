7c539e3447960da770c36a467d0902f1c360be43
tanvee-dadhaneeya <tanvee.dadhaneeya@accenture.com>
2024-11-05 12:52:37 +0530

    commit change for show live preview

--- app/components/RequestUnlockModal.tsx
+++ app/components/RequestUnlockModal.tsx
@@ -13,6 +13,29 @@ import { useCheckOutData } from "@/app/hooks/useCheckOutData";
 const RequestUnlockModal = (props: any) => {
     const { currentMetaData } = props;
     const { contextData } = useCheckOutData();
+    const openIframe = () => {
+        const newTab = window.open('', '_blank');
+        if (newTab) {
+          newTab.document.write(`
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <title>Iframe Example</title>
+                <style>
+                  body { margin: 0; }
+                  iframe { width: 100%; height: 100vh; border: none; }
+                </style>
+              </head>
+              <body>
+                <iframe src="https://supportcenter.corp.google.com/preview/techstop?origin=gcp-na-app.contentstack.com&%2Ftechstop%2Farticle%2Fbltb6d6c5ab0a7f1cf9="></iframe>
+              </body>
+            </html>
+          `);
+          newTab.document.close(); // Close the document to render the content
+        } else {
+          alert('Please allow popups for this website');
+        }
+      };
     return (
         <>
             <ModalHeader title="Entry Locked" />
@@ -24,7 +47,7 @@ const RequestUnlockModal = (props: any) => {
             <ModalFooter>
                 <ButtonGroup>
                     <Button buttonType="primary">Request to unlock</Button>
-                    <Button buttonType="primary">View Live Preview</Button>
+                    <Button buttonType="primary" onClick={openIframe}>View Live Preview</Button>
                     <Button buttonType="primary" onClick={() => {
                         document.location.href = `https://app.contentstack.com/#!/stack/${contextData?.api_key}/dashboard?branch=${contextData?.branch}`; 
                     }}>Back to Dashboard</Button>
