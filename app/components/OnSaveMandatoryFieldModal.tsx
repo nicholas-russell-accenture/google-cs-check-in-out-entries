85309c23c6f144c64a4505ad903a4d8fc6fe1749
Rudragouda Patil <rudragouda.patil@accenture.com>
2025-03-05 22:57:26 +0530

    disabeling on selct option shown on drop down

--- app/components/OnSaveMandatoryFieldModal.tsx
+++ app/components/OnSaveMandatoryFieldModal.tsx
@@ -63,12 +63,12 @@ const OnSaveMandatoryFieldModal: React.FC<OnSaveMandatoryFieldProps> = (props) =
         <div>
            <p>
             <label htmlFor="audience" className="block text-sm font-medium text-gray-700 mb-2">
-              <h3>Please select an Audience Value for This Entry.</h3>
+              <h3>Select the main audience for your article, Select Googlers for articles meant for all Googlers and Resolvers for articles meant for technicians only.</h3>
               {/*audienceData?.schema[0]?.display_name */}
             </label>
             <select id="audience" className="block w-1/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                     value={selectedAudience} onChange={handleChange}>
-              <option value="">Click to select options</option>
+              <option value="" selected disabled hidden>Click to select options</option>
               {audienceOptions.map((audience:any) => (
                 <option key={audience.value} value={audience.value}>
                   {audience.value}
